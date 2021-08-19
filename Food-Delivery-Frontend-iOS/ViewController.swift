ac8a7aab7b44274b5a3b63cc4010892247ba9874
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-08-19 20:25:45 +0200

    first commit

---
+++ Food-Delivery-Frontend-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food-Delivery-Frontend-iOS
+//
+//  Created by Bülent Barış Kılıç on 19.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
