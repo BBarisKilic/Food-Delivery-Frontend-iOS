3822981a57fc4249cb831a39e9ae07b7fbf458fa
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-08-21 22:15:46 +0200

    Made some changes on TabBar.

--- Food-Delivery-Frontend-iOS/Controller/TabBarController.swift
+++ Food-Delivery-Frontend-iOS/Controller/TabBarController.swift
@@ -12,7 +12,20 @@ class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tabBar.tintColor = .white
-        tabBar.unselectedItemTintColor = .gray
+        let appearanceNavigationBar = UINavigationBar.appearance()
+        appearanceNavigationBar.barTintColor = UIColor(hex: 0xFFFA4A0C)
+        
+        self.tabBar.tintColor = .white
+        self.tabBar.unselectedItemTintColor = .white.withAlphaComponent(0.8)
+        
+        
+        let appearance = UITabBarItem.appearance()
+        let attributes = [NSAttributedString.Key.font:UIFont(name: "Arial Hebrew", size: 20)]
+        appearance.setTitleTextAttributes(attributes as [NSAttributedString.Key : Any], for: .normal)
+        let tabBarItems = tabBar.items! as [UITabBarItem]
+        
+        tabBarItems[0].title = "Main"
+        tabBarItems[1].title = "Cart"
+        tabBarItems[2].title = "More"
     }
 }
---
+++ Food-Delivery-Frontend-iOS/Extension/UIColorExt.swift
@@ -0,0 +1,20 @@
+//
+//  UIColorExt.swift
+//  Food-Delivery-Frontend-iOS
+//
+//  Created by Bülent Barış Kılıç on 21.08.2021.
+//
+
+import UIKit
+
+extension UIColor {
+
+    convenience init(hex: Int) {
+        let components = (
+            R: CGFloat((hex >> 16) & 0xff) / 255,
+            G: CGFloat((hex >> 08) & 0xff) / 255,
+            B: CGFloat((hex >> 00) & 0xff) / 255
+        )
+        self.init(red: components.R, green: components.G, blue: components.B, alpha: 1)
+    }
+}
