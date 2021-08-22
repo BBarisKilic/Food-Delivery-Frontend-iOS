55472684e07709966ba0fc53a46f59feb6ed0cd6
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-08-22 17:13:55 +0200

    Shared Data created.

---
+++ Food-Delivery-Frontend-iOS/Extension/CartViewExt.swift
@@ -0,0 +1,8 @@
+//
+//  CartViewExt.swift
+//  Food-Delivery-Frontend-iOS
+//
+//  Created by Bülent Barış Kılıç on 22.08.2021.
+//
+
+import Foundation
---
+++ Food-Delivery-Frontend-iOS/Model/Cart.swift
@@ -0,0 +1,8 @@
+//
+//  Cart.swift
+//  Food-Delivery-Frontend-iOS
+//
+//  Created by Bülent Barış Kılıç on 22.08.2021.
+//
+
+import Foundation
